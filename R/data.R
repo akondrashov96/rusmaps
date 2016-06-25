3cc24bcd7b91ea7c51e31bcb9eb0d4f287080fe7
Artem Kondrashov <Artem@MacBook-Pro-MacBook-2.local>
2016-06-26 01:54:31 +0300

    Added cities with over 1 mil population maps

--- R/data.R
+++ R/data.R
@@ -15,12 +15,210 @@
 #'
 #' The dataset contains names, population (as of 2010) and administrative level of included regions
 #'
-#' @format A SpatialPointsDataFrame object with 15 points with following attibutes:
+#' @format A SpatialPointsDataFrame object with 6 polygons with following attibutes:
 #' \describe{
 #'  \item{id}{custom id assigned}
 #'  \item{name}{region name}
-#'  \item{admin_lvl}{Administrative level of the region}
-#'  \item{pop_2015}{population in 2010, according to Rosstat}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2010}{population in 2010, according to Rosstat}
 #'  }
 #' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
 "Voronezh"
+
+#' Administrative regions of Nizhniy Novgorod city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 8 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Novgorod"
+
+#' Administrative regions of Novosibirsk city
+#'
+#' The dataset contains names, population (as of 2016) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 10 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2016}{population in 2016, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Novosibirsk"
+
+#' Administrative regions of Omsk city
+#'
+#' The dataset contains names, population (as of 2010) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 5 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2010}{population in 2010, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Omsk"
+
+#' Administrative regions of Perm city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 9 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Perm"
+
+#' Administrative regions of Rostov-na-Donu city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 8 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Rostov"
+
+#' Administrative regions of Samara city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 9 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Samara"
+
+#' Administrative regions of Saint-Petersburg city
+#'
+#' The dataset contains names, population (as of 2015 and 2016) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 18 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2016}{population in 2016, according to Rosstat}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"StPetersburg"
+
+#' Administrative regions of Ufa city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 7 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Ufa"
+
+#' Administrative regions of Chelyabinsk city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 7 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Chelyabinsk"
+
+#' Administrative regions of Moscow city
+#'
+#' The dataset contains names, population (as of 2015 and 2016) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 12 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2016}{population in 2016, according to Rosstat}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Moscow"
+
+#' Administrative regions of Krasnoyarsk city
+#'
+#' The dataset contains names, population (as of 2010) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 7 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2010}{population in 2010, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Krasnoyarsk"
+
+#' Administrative regions of Kazan city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 7 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Kazan"
+
+#' Administrative regions of Ekaterinburg city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 7 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Ekaterinburg"
+
+#' Administrative regions of Volgograd city
+#'
+#' The dataset contains names, population (as of 2015) and administrative level of included regions
+#'
+#' @format A SpatialPointsDataFrame object with 8 polygons with following attibutes:
+#' \describe{
+#'  \item{id}{custom id assigned}
+#'  \item{name}{region name}
+#'  \item{admin_lvl}{administrative level of the region}
+#'  \item{pop_2015}{population in 2015, according to Rosstat}
+#'  }
+#' @source Obtained with QGIS using OpenStreetMap data. Data tables were customized. Population data retrieved from Rosstat.
+"Volgograd"
